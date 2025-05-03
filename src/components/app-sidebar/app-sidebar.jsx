8b89d3ea42ffde6010d04f35287701aeb83d3dca
Shohaib hossain <khanshohaibhossain@gmail.com>
2025-05-03 06:36:13 +0600

    resolve the issue

--- src/components/app-sidebar/app-sidebar.jsx
+++ src/components/app-sidebar/app-sidebar.jsx
@@ -15,7 +15,7 @@ import {
 const items = [
   {
     title: "Home",
-    url: "#",
+    url: "/dashboard",
     icon: Home,
   },
   {
@@ -35,7 +35,7 @@ const items = [
   },
   {
     title: "Settings",
-    url: "#",
+    url: "/settings",
     icon: Settings,
   },
 ];
